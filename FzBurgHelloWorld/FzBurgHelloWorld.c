12d1f114f5c68ac8ebe93998423522821c887d11
fhh200000 <1041154549@qq.com>
2020-02-10 20:12:53 +0800

    Update:Hello world.

---
+++ FzBurgHelloWorld/FzBurgHelloWorld.c
@@ -0,0 +1,8 @@
+#include <Uefi.h>
+EFI_STATUS
+EFIAPI
+UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
+{
+    SystemTable->ConOut->OutputString(SystemTable->ConOut,L"HelloWorld\n");
+    return EFI_SUCCESS;
+}
